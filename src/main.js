d1db3651e1b0e3cf804e0b7cfee4c2f14157506f
Maxim Rudnitskiy <rydnizkui@gmail.com>
2024-07-23 23:18:03 +0300

    before merge with main

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import directives from '@/directives';
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://broker-service-pardo.onrender.com'
 
 const app = createApp(App)
 
