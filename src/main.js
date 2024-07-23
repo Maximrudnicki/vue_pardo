83cfe14cf91eef05f32f4ef7a9eecb2098e1deb7
Maxim Rudnitskiy <rydnizkui@gmail.com>
2024-07-23 23:20:42 +0300

    connect to monolith (to reduce costs)

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import directives from '@/directives';
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://broker-service-pardo.onrender.com'
+axios.defaults.baseURL = 'https://mono-pardo.onrender.com'
 
 const app = createApp(App)
 
